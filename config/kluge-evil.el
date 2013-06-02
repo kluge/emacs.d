20f5ec4166e797cd8beabd5bcb18005dc15536a6
Kari Oikarinen <kluge@kaanto.net>
2013-06-02 10:36:16 +0300

    Bind M-d for scrolling up

--- config/kluge-evil.el
+++ config/kluge-evil.el
@@ -3,4 +3,7 @@
 ;; C-e for end of line in insert state
 (define-key evil-insert-state-map (kbd "C-e") 'evil-end-of-line)
 
+;; M-d for scrolling up
+(define-key evil-normal-state-map (kbd "M-d") 'evil-scroll-up)
+
 (provide 'kluge-evil)
