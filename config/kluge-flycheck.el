26544bc031dcc63adde97108d016992239eb2981
Kari Oikarinen <kluge@kaanto.net>
2013-06-09 12:01:38 +0300

    flycheck: Don't complain about elisp comments

--- config/kluge-flycheck.el
+++ config/kluge-flycheck.el
@@ -1,4 +1,9 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;; Don't complain about comment structure in elisp files
+;; (from http://stackoverflow.com/questions/15552349/flycheck-how-to-disable-warnning-while-edit-emacs-lisp-scripts)
+(eval-after-load 'flycheck
+  '(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
+
 (provide 'kluge-flycheck)
