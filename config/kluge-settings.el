d38b578d5394612b4aedc0a90647baf2a8337251
Kari Oikarinen <kluge@kaanto.net>
2013-06-09 12:38:37 +0300

    Don't use tabs in aligning

--- config/kluge-settings.el
+++ config/kluge-settings.el
@@ -6,13 +6,20 @@
           `((".*" ,temporary-file-directory t)))
 
 ;; Visual settings
-(show-paren-mode 1)	; highlight matching parenthesis
+(show-paren-mode 1)     ; highlight matching parenthesis
 (global-hl-line-mode 1) ; highlight current line
 (line-number-mode 1)    ; show line number in modeline
-(column-number-mode 1)	; show column number in modeline
+(column-number-mode 1)  ; show column number in modeline
 (global-linum-mode -1)  ; don't show line numbers
 
 ;; Code settings
 (setq indent-tabs-mode nil) ; use spaces to indent
+; don't use tabs in align-regexp
+; (from http://stackoverflow.com/questions/915985/in-emacs-how-to-line-up-equals-signs-in-a-series-of-initialization-statements)
+(defadvice align-regexp (around align-regexp-with-spaces)
+  "Never use tabs for alignment."
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+(ad-activate 'align-regexp)
 
 (provide 'kluge-settings)
