fd1c6852133928ac3814fff48c9e8051c0d4baf5
Kari Oikarinen <kluge@kaanto.net>
2013-08-03 10:51:29 +0300

    Haskell indent offset to 4, don't warn in tab cycle

--- config/kluge-haskell.el
+++ config/kluge-haskell.el
@@ -1,5 +1,9 @@
 (require 'haskell-mode)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(setq haskell-indentation-layout-offset 4)
+(setq haskell-indentation-left-offset 4)
+(setq haskell-indentation-ifte-offset 4)
+(setq haskell-indentation-cycle-warn nil) ; don't warn about moving to leftmost position
 
 (provide 'kluge-haskell)
