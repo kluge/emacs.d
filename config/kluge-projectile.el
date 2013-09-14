eb94d217fdf24cbc815473783dd9a8eac92d502f
Kari Oikarinen <kluge@kaanto.net>
2013-09-14 14:00:18 +0300

    Some shorter keys for projectile

---
+++ config/kluge-projectile.el
@@ -0,0 +1,9 @@
+(require 'projectile)
+(projectile-global-mode)
+
+; Shorter keys
+(define-key evil-normal-state-map (kbd "M-p f") 'projectile-find-file)
+(define-key evil-normal-state-map (kbd "M-p a") 'projectile-ack)
+(define-key evil-normal-state-map (kbd "M-p b") 'projectile-switch-to-buffer)
+
+(provide 'kluge-projectile)
