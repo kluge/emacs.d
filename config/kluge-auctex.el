b74c1ce57ea4c1aae2adbab4d72aa33e02fa0688
Kari Oikarinen <kluge@kaanto.net>
2013-09-25 11:18:26 +0300

    auctex: enable math mode and save before compile

--- config/kluge-auctex.el
+++ config/kluge-auctex.el
@@ -8,8 +8,14 @@
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (setq reftex-plug-into-AUCTex t)
 
+;; Use mathmode
+(setq LaTeX-math-abbrev-prefix "§")
+(add-hook 'TeX-mode-hook 'LaTeX-math-mode)
+
 (add-to-list 'evil-emacs-state-modes 'reftex-select-label-mode)
 
+;; Save before compiling without asking
+(setq TeX-save-query nil)
 ;; Compile into PDFs
 (setq TeX-PDF-mode t)
 ;; Use Okular to view
