3616f1ddb17825e63a2b6bb8a24f003206504588
Kari Oikarinen <kluge@kaanto.net>
2013-10-20 11:20:16 +0300

    Clean up ace-jump bindings, motion state is enough

--- config/kluge-ace-jump.el
+++ config/kluge-ace-jump.el
@@ -2,10 +2,8 @@
 
 ;; Keybindings for Evil integration
  
-(define-key evil-normal-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
-(define-key evil-visual-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
 (define-key evil-motion-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
-(define-key evil-operator-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
-(define-key evil-operator-state-map (kbd "C-SPC") 'evil-ace-jump-char-to-mode)
+(define-key evil-motion-state-map (kbd "C-SPC") 'evil-ace-jump-char-to-mode)
+(define-key evil-motion-state-map (kbd "g SPC") 'evil-ace-jump-line-mode)
 
 (provide 'kluge-ace-jump)
