2aaf33a273f4975344ad523a9ead9ed2788982db
Kari Oikarinen <kluge@kaanto.net>
2013-10-21 17:24:12 +0300

    Use haskell-decl-scan and haskell-doc

--- config/kluge-haskell.el
+++ config/kluge-haskell.el
@@ -1,9 +1,20 @@
 (require 'haskell-mode)
 
+;; Use the newest indentation mode
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (setq haskell-indentation-layout-offset 4)
 (setq haskell-indentation-left-offset 4)
 (setq haskell-indentation-ifte-offset 4)
 (setq haskell-indentation-cycle-warn nil) ; don't warn about moving to leftmost position
+
+;; Scan declarations for imenu
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+
+;; Show type signatures in the minibuffer
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc)
+
+;; Use ghc-mod
+(require 'ghc)
+(add-hook 'haskell-mode-hook 'ghc-init)
 
 (provide 'kluge-haskell)
