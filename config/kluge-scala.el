291baf320f8f9e1b0df1a61ba6ffc290a7d81fd6
Kari Oikarinen <kluge@kluge.fi>
2013-11-21 23:00:07 +0200

    Make {<RET> put } on the next line in Scala

--- config/kluge-scala.el
+++ config/kluge-scala.el
@@ -7,4 +7,13 @@
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
+(eval-after-load 'kluge-smartparens
+  '(sp-local-pair 'scala-mode "{" nil :post-handlers '((kluge-block-after-newline "RET"))))
+
+(defun kluge-block-after-newline (&rest _ignored)
+  "Indent the trailing } on a separate line after the cursor."
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
 (provide 'kluge-scala)
