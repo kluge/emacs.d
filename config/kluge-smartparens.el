b76ec6a7b7663d42d3f2291b4f55a1687b1d13ca
Kari Oikarinen <kluge@kluge.fi>
2014-04-17 23:05:49 +0300

    Keybindings for slurping and barfing

--- config/kluge-smartparens.el
+++ config/kluge-smartparens.el
@@ -1,5 +1,10 @@
 (use-package smartparens-config
   :init	
-  (smartparens-global-mode t))
+  (progn
+    (smartparens-global-mode t)
+    (define-key evil-normal-state-map (kbd "M-l") 'sp-forward-slurp-sexp)
+    (define-key evil-normal-state-map (kbd "M-h") 'sp-forward-barf-sexp)
+    (define-key evil-normal-state-map (kbd "M-L") 'sp-backward-barf-sexp)
+    (define-key evil-normal-state-map (kbd "M-H") 'sp-backward-slurp-sexp)))
 
 (provide 'kluge-smartparens)
