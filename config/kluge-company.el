69c83adc1967faf79a7ea2398a01fd6284ec0866
Kari Oikarinen <kluge@kluge.fi>
2014-11-18 17:53:40 +0200

    Use C-l instead of C-w for company's location info

--- config/kluge-company.el
+++ config/kluge-company.el
@@ -3,6 +3,11 @@
   (progn
     (setq company-idle-delay t)
     (setq company-minimum-prefix-length 2)
-    (global-company-mode 1)))
+    (global-company-mode 1))
+  :config
+  (progn
+    ;; Move show location to C-l
+    (define-key company-active-map (kbd "C-w") nil)
+    (define-key company-active-map (kbd "C-l") 'company-show-location)))
 
 (provide 'kluge-company)
