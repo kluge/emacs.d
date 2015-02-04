d463b588e7aee64656f51b861598d8682de51e85
Kari Oikarinen <kluge@kluge.fi>
2015-02-04 16:02:05 +0200

    company: Set delay to 0 instead of t

--- config/kluge-company.el
+++ config/kluge-company.el
@@ -1,7 +1,7 @@
 (use-package company
   :init
   (progn
-    (setq company-idle-delay t)
+    (setq company-idle-delay 0)
     (setq company-minimum-prefix-length 2)
     (global-company-mode 1))
   :config
