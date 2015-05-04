60b5365170f964829274f5d5cd7d2a93659fb7cf
Kari Oikarinen <kluge@kluge.fi>
2015-05-04 09:15:35 +0300

    .gitignore updates and utf-8 for Windows

--- config/kluge-flycheck.el
+++ config/kluge-flycheck.el
@@ -1,9 +1,9 @@
 (use-package flycheck
   :ensure t
   :config
-  (global-flycheck-mode)
   ;; Don't complain about comment structure in elisp files
   ;; (from http://stackoverflow.com/questions/15552349/flycheck-how-to-disable-warnning-while-edit-emacs-lisp-scripts)
-  (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers)))
+  (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
+  (global-flycheck-mode))
 
 (provide 'kluge-flycheck)
