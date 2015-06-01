8b203eff8bdcec99bc0674c7d422d47b5e562bf8
Kari Oikarinen <kluge@kluge.fi>
2015-06-01 15:33:18 +0300

    Guard things requiring at least Emacs 24.4

--- config/kluge-paradox.el
+++ config/kluge-paradox.el
@@ -1,6 +1,7 @@
-(use-package paradox
-  :ensure t
-  :init
-  (evil-set-initial-state 'paradox-menu-mode 'emacs))
+(unless (version< emacs-version "24.4")
+  (use-package paradox
+    :ensure t
+    :init
+    (evil-set-initial-state 'paradox-menu-mode 'emacs)))
 
 (provide 'kluge-paradox)
