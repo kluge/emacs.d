d329d97b29a7b6618ae8503f383234dd2a4f7ecf
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 22:59:57 +0300

    Make <f2> toggle ansi-term buffer
    And C-w <f2> to open an ansi-term in a split.

--- config/kluge-ansi-term.el
+++ config/kluge-ansi-term.el
@@ -1,3 +1,5 @@
+(require 'term)
+
 ;; Don't enable yasnippet in ansi-term
 (add-hook 'term-mode-hook (lambda ()
 			    (yas-minor-mode -1)))
@@ -5,10 +7,10 @@
 ;; Use emacs state in ansi-term
 (evil-set-initial-state 'term-mode 'emacs)
 
-;; From http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
-(defun visit-ansi-term ()
+;; Adapted from http://www.enigmacurry.com/2008/12/26/emacs-ansi-term-tricks/
+(defun kluge-visit-ansi-term ()
   "If the current buffer is:
-     1) a running ansi-term named *ansi-term*, rename it.
+     1) a running ansi-term named *ansi-term*, switch to previous buffer
      2) a stopped ansi-term, kill it and create a new one.
      3) a non ansi-term, go to an already running ansi-term
         or start a new one while killing a defunct one"
@@ -20,7 +22,7 @@
     (if is-term
         (if is-running
             (if (string= "*ansi-term*" (buffer-name))
-                (call-interactively 'rename-buffer)
+                (switch-to-prev-buffer)
               (if anon-term
                   (switch-to-buffer "*ansi-term*")
                 (ansi-term term-cmd)))
@@ -32,7 +34,13 @@
             (kill-buffer "*ansi-term*")
             (ansi-term term-cmd))
         (ansi-term term-cmd)))))
-(global-set-key (kbd "<f2>") 'visit-ansi-term)
+(global-set-key (kbd "<f2>") 'kluge-visit-ansi-term)
+
+(defun kluge-split-ansi-term ()
+  (interactive)
+  (kluge-horizontal-split)
+  (kluge-visit-ansi-term))
+(define-key evil-window-map (kbd "<f2>") 'kluge-split-ansi-term)
 
 
 
