3973eb0fe9f101907e859c8d0f2331a7d1a165b5
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 15:26:04 +0300

    Use a newer C++ standard in flycheck

--- config/kluge-flycheck.el
+++ config/kluge-flycheck.el
@@ -1,12 +1,12 @@
 (defun kluge-set-flycheck-clang-std ()
-  (setq-local flycheck-clang-language-standard "c++11"))
+  (setq-local flycheck-clang-language-standard "c++14"))
 
 (use-package flycheck
   :ensure t
   :config
   ;; Don't complain about comment structure in elisp files
   ;; (from http://stackoverflow.com/questions/15552349/flycheck-how-to-disable-warnning-while-edit-emacs-lisp-scripts)
-  (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
+  (delete-from-list 'flycheck-checkers 'emacs-lisp-checkdoc)
   (add-hook 'c++-mode-hook 'kluge-set-flycheck-clang-std)
   (global-flycheck-mode))
 
