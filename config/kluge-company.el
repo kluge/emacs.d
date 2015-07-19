3be9248036caea95f7c6891ee23a173312f42123
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 16:19:31 +0300

    Use C-n and C-p to move between company completions

--- config/kluge-company.el
+++ config/kluge-company.el
@@ -1,14 +1,18 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0)
+  (setq company-idle-delay 0.2)
   (setq company-minimum-prefix-length 2)
-
-  ;; Don't use semantic
-  (delete 'company-semantic company-backends)
 
   ;; Case-sensitive completion in plain text
   (setq company-dabbrev-downcase nil)
+  (setq company-dabbrev-ignore-case nil)
+
+  ;; Use company instead of evil's completion
+  (define-key evil-insert-state-map (kbd "C-n") nil)
+  (define-key evil-insert-state-map (kbd "C-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
   ;; Move show location to C-l
   (define-key company-active-map (kbd "C-w") nil)
