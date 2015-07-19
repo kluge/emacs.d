0d5928ce7504b33aa77c2302a4e740517b44a2c9
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 15:25:31 +0300

    Set smartparens keys in a wider context

--- config/kluge-smartparens.el
+++ config/kluge-smartparens.el
@@ -1,10 +1,10 @@
 (use-package smartparens-config
   :ensure smartparens
   :config	
-  (define-key evil-normal-state-map (kbd "M-l") 'sp-forward-slurp-sexp)
-  (define-key evil-normal-state-map (kbd "M-h") 'sp-forward-barf-sexp)
-  (define-key evil-normal-state-map (kbd "M-L") 'sp-backward-barf-sexp)
-  (define-key evil-normal-state-map (kbd "M-H") 'sp-backward-slurp-sexp)
+  (global-set-key (kbd "M-l") 'sp-forward-slurp-sexp)
+  (global-set-key (kbd "M-h") 'sp-forward-barf-sexp)
+  (global-set-key (kbd "M-L") 'sp-backward-barf-sexp)
+  (global-set-key (kbd "M-H") 'sp-backward-slurp-sexp)
 
   (smartparens-global-mode 1))
 
