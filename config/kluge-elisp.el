c7e0dd1bc55daeb49cc8ac5b202d06df8bb100a8
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 15:04:20 +0300

    Use elisp-slime-nav

--- config/kluge-elisp.el
+++ config/kluge-elisp.el
@@ -3,16 +3,26 @@
 	  (lambda ()
 	    (setq evil-shift-width 2)))
 
+(defvar kluge-elisp-mode-hooks '(emacs-lisp-mode-hook
+                                 lisp-interaction-mode-hook
+                                 ielm-mode-hook))
+
 (use-package eldoc
   :diminish eldoc-mode
   :config
   ;; Enable eldoc (show function arguments in the minibuffer)
-  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode))
+  (dolist (hook kluge-elisp-mode-hooks)
+    (add-hook 'hook 'turn-on-eldoc-mode)))
 
-(evil-leader/set-key
-  "e" 'eval-last-sexp
-  "E" 'eval-defun)
+(dolist (mode '(emacs-lisp-mode lisp-interaction-mode))
+  (evil-leader/set-key-for-mode mode
+    "e" 'eval-last-sexp
+    "E" 'eval-defun))
+
+(use-package elisp-slime-nav
+  :ensure t
+  :init
+  (dolist (hook kluge-elisp-mode-hooks)
+    (add-hook hook 'turn-on-elisp-slime-nav-mode)))
 
 (provide 'kluge-elisp)
