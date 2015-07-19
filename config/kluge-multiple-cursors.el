f56aff9f34d710aedc9cb9be6a23ff135c3204c9
Kari Oikarinen <kluge@kluge.fi>
2015-07-19 21:52:20 +0300

    Fix switching to emacs state for multiple-cursors
    Newer multiple-cursors seems to sometimes leave the mark active which
    means disabling the region is necessary.
    
    The previous handling also sometimes stayed in emacs state for reasons I
    couldn't figure out. But always switching back to normal state is pretty
    expected anyway.

--- config/kluge-multiple-cursors.el
+++ config/kluge-multiple-cursors.el
@@ -10,26 +10,12 @@
 
   ;; Functions to change to Emacs state and back around multiple-cursors
   ;; invocations, since multiple-cursors does not support Evil
-  (add-hook 'multiple-cursors-mode-enabled-hook 'kluge-mc-store-state-and-emacs)
-  (add-hook 'multiple-cursors-mode-disabled-hook 'kluge-mc-restore-state))
+  (add-hook 'multiple-cursors-mode-enabled-hook 'evil-emacs-state)
+  (add-hook 'multiple-cursors-mode-disabled-hook 'kluge-mc-restore-normal-state))
 
-(defvar kluge-mc-old-state nil
-  "Stores the state we were in before enabling multiple-cursors.")
-(defvar kluge-mc nil
-  "Whether we already stored the state for this invocation of
-  multiple-cursors.")
-
-(defun kluge-mc-store-state-and-emacs ()
-  "Store the state we were in before enabling multiple-cursors
-and change to Emacs state."
-  (unless kluge-mc
-    (setq kluge-mc-old-state evil-state)
-    (setq kluge-mc t)
-    (evil-emacs-state)))
-
-(defun kluge-mc-restore-state ()
-  "Restore the state we were in before enabling multiple-cursors."
-  (setq kluge-mc nil)
-  (evil-change-state kluge-mc-old-state))
+(defun kluge-mc-restore-normal-state ()
+  "Restore evil normal state after multiple-cursors."
+  (setq mark-active nil) ; multiple-cursors may leave region active
+  (evil-force-normal-state))
 
 (provide 'kluge-multiple-cursors)
