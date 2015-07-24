aeaace5f1dc8e3ea7c6c757302d6c81aa1d87d43
Kari Oikarinen <kluge@kluge.fi>
2015-07-24 17:25:40 +0300

    Only use locally defined yasnippet snippets

--- config/kluge-utils.el
+++ config/kluge-utils.el
@@ -2,6 +2,6 @@
 
 (defun delete-from-list (list-var item)
   "Delete item from a list variable."
-  (setq list-var (delete item (symbol-value list-var))))
+  (set list-var (delete item (symbol-value list-var))))
 
 (provide 'kluge-utils)
--- config/kluge-yasnippet.el
+++ config/kluge-yasnippet.el
@@ -2,9 +2,12 @@
   :ensure t
   :defer 5
   :config
+  ;; Don't use GTK dropdowns
+  (delete-from-list 'yas-prompt-functions 'yas-x-prompt)
+  ;; Only use my own snippets
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
   ;; Less verbosity
   (setq yas-verbosity 1)
   (yas-global-mode 1))
-
 
 (provide 'kluge-yasnippet)
