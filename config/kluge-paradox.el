7c21072e974d6a93ee1aca5ed3583cd6910dcd32
Kari Oikarinen <kluge@kluge.fi>
2015-10-03 21:39:10 +0300

    Lazy load paradox

--- config/kluge-paradox.el
+++ config/kluge-paradox.el
@@ -1,7 +1,8 @@
 (unless (version< emacs-version "24.4")
   (use-package paradox
     :ensure t
-    :init
+    :defer t
+    :config
     (evil-set-initial-state 'paradox-menu-mode 'emacs)))
 
 (provide 'kluge-paradox)
