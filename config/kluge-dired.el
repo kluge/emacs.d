f19abdd6cfeef0d1eb38ced50c452895402d6687
Kari Oikarinen <kluge@kluge.fi>
2015-10-04 16:05:32 +0300

    dired: Don't prompt for copying directories

--- config/kluge-dired.el
+++ config/kluge-dired.el
@@ -3,6 +3,7 @@
   :config
   (setq-default dired-listing-switches "-alh")
   (setq dired-dwim-target t) ; Suggest targets from the other open dired buffer
+  (setq dired-recursive-copies 'always) ; Don't prompt for copying directories
   (define-key dired-mode-map (kbd "-") 'dired-up-directory))
 
 (provide 'kluge-dired)
