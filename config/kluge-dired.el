5a027d22076131b46a5d747c87aeb85a83e50eb0
Kari Oikarinen <kluge@kluge.fi>
2015-10-04 13:42:28 +0300

    Suggest targets from the other open dired buffer

--- config/kluge-dired.el
+++ config/kluge-dired.el
@@ -2,6 +2,7 @@
   :ensure t
   :config
   (setq-default dired-listing-switches "-alh")
+  (setq dired-dwim-target t) ; Suggest targets from the other open dired buffer
   (define-key dired-mode-map (kbd "-") 'dired-up-directory))
 
 (provide 'kluge-dired)
